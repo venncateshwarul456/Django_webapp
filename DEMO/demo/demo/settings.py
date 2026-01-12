2f6e34661e3e5bd243ba574ab264b7bd2842bc8c
venncateshwarul456 <venncateshwarul@yahoo.com>
2026-01-12 12:00:39 +0530

    modified 2 files

--- DEMO/demo/demo/settings.py
+++ DEMO/demo/demo/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "myapp",
+    'myapp.apps.MyappConfig',
 ]
 
 STATIC_URL = '/static/'
@@ -122,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'

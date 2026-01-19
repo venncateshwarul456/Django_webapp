5b12ed402b2dbac81784ff891752243471b2fffe
venncateshwarul456 <venncateshwarul@yahoo.com>
2026-01-19 13:29:06 +0530

    modified files

---
+++ DEMO/demo/demo/demo/myapp/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+
+
+from django.db import models
+
+class TodoItem(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    is_completed = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
